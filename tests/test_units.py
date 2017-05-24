2daca77594507b4a1990e68e04430d4f1bd0a16b
Jim Bosch <jbosch@astro.princeton.edu>
2017-05-24 13:38:59 -0400

    Slightly refactor SQL usage into a separate class.
    This probably won't enough to really support multiple backends, but
    we should at least avoid tying the core classes to SQLite details.

--- tests/test_units.py
+++ tests/test_units.py
@@ -4,23 +4,22 @@ from __future__ import print_function, division, absolute_import
 
 import sys
 import os
+import unittest
+
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-import unittest
-import sqlite3
 import repodb
 
 
 class UnitsTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db = sqlite3.connect(":memory:")
+        self.backend = repodb.SqliteBackend(":memory:")
 
     def testCreateTables(self):
         for UnitClass in repodb.COMMON_UNITS:
-            print(repodb.sqlCreateTable(UnitClass))
-            self.db.execute(repodb.sqlCreateTable(UnitClass))
-        self.db.commit()
+            self.backend.createTable(UnitClass)
+        self.backend.commit()
 
 
 if __name__ == "__main__":
